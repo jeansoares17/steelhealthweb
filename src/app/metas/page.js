ecc086bf9ad48ebedb5254a1e647363e2f4419f4
Jean Soares <jeansoaresferreira72@gmail.com>
2023-09-04 20:15:29 -0300

    busca

--- src/app/metas/page.js
+++ src/app/metas/page.js
@@ -1,12 +1,27 @@
 import NavBar from "@/components/NavBar";
+import Metas from "./Metas";
 
-export default function Metas() {
+async function getMetas(){
+  const url = "http://localhost:8080/api/metas"
+  const resp = await fetch(url, { next: { revalidate: 0 } })
+  return resp.json()
+}
+
+export default async function Metas() {
+
+  const data = await getMetas()
+
   return (
     <>
       <NavBar active={"metas"} />
 
-      <main>
-        <h1>Metas</h1>
+      <main className="bg-slate-900 mt-10 m-auto max-w-lg p-8 rounded">
+        <h1 className="text-2xl">Metas</h1>
+
+        <div id="data">
+          {data.map(meta => <Metas key={meta.id} meta={meta} />)}
+        </div>
+
       </main>
     </>
 
