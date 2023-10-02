aa0ed74f41aef5b2871350d853378b470de950d7
Jean Soares <jeansoaresferreira72@gmail.com>
2023-10-02 14:54:39 -0300

    jwt

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,6 +1,20 @@
+"use client"
+
+import { AuthContext } from "@/contexts/AuthContext";
 import Link from "next/link";
+import { useContext } from "react";
+import Button from "./Button";
+import { useRouter } from "next/navigation";
 
 export default function NavBar({ active }) {
+    const {user, logout} = useContext(AuthContext)
+    const { push } = useRouter()
+
+    function handleLogout(){
+        logout()
+        push("/login")
+    }
+
     return (
         <nav className=" flex justify-between items-center py-2 px-4 bg-slate-900">
             <ul id="links" className="flex gap-14 items-end text-slate-500">
@@ -10,27 +24,35 @@ export default function NavBar({ active }) {
                     </Link>
                 </li>
                 <li>
-                    <Link className={active=="treinos" && "text-slate-300"} href="/treino">
-                    treino
+                    <Link className={active == "treinos" && "text-slate-300"} href="/treinos">
+                        Treinos
                     </Link>
                 </li>
-                
+
                 <li>
-                    <Link className={active=="metas" && "text-slate-300"} href="/meta">
-                        meta
+                    <Link className={active == "metas" && "text-slate-300"} href="/metas">
+                        metas
                     </Link>
                 </li>
-                
+
                 <li>
-                    <Link className={active=="alimentacao" && "text-slate-300"} href="/alimentacao">
-                        alimentação
+                    <Link className={active == "alimentacoes" && "text-slate-300"} href="/alimentacoes">
+                        alimentações
                     </Link>
-                </li>
-                
+                </li>              
+
             </ul>
 
-            <div className="h-12 w-12 rounded-full overflow-hidden">
-                <img src="https://static.poder360.com.br/2021/08/messi-contrato-barcelona.jpg" alt="avatar do usuário" />
+            <div className="flex items-center gap-2">
+                <span>{user?.email}</span>
+
+                <div className="h-12 w-12 rounded-full overflow-hidden">
+                    <img src="https://i.pravatar.cc/100" alt="avatar do usuário" />
+                </div>
+
+                <Button onClick={handleLogout} type="button">logout</Button>
+
+
             </div>
 
         </nav>
